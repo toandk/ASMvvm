7e5bc8c27ccfbd1f19ac0177e5eba7c315f59907
starfall_9000 <starfall.9000.21@gmail.com>
2021-06-21 09:54:51 +0700

    remove Pods folder

--- Pods/Target Support Files/ASMvvm/ASMvvm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ASMvvm : NSObject
-@end
-@implementation PodsDummy_ASMvvm
-@end
--- Pods/Target Support Files/RxASDataSources/RxASDataSources-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxASDataSources : NSObject
-@end
-@implementation PodsDummy_RxASDataSources
-@end
